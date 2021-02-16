ded3a93ce845df3080a50ac663ee1ecd71ed8b05
Zehra Nur Koyuncu <zehra.koyuncu@metu.edu.tr>
2021-02-16 18:22:42 +0300

    Add project files.

---
+++ MyDictionaryHomework/MyDictionary.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionaryHomework
+{
+    class MyDictionary<T, V>
+    {
+        T[] key;
+        V[] value;
+
+        public MyDictionary()
+        {
+            key = new T[0];
+            value = new V[0];
+
+        }
+        public void Add(T t, V v)
+        {
+            T[] tempArrayKey = key;
+            V[] tempArrayValue = value;
+            key = new T[key.Length + 1];
+            value = new V[value.Length + 1];
+
+            for (int i = 0; i < tempArrayKey.Length; i++)
+            {
+                key[i] = tempArrayKey[i];
+            }
+
+            for (int i = 0; i < tempArrayValue.Length; i++)
+            {
+                value[i] = tempArrayValue[i];
+            }
+
+            key[key.Length - 1] = t;
+            value[value.Length - 1] = v;
+
+            Console.WriteLine("Kitap Yazarı : " + t + " | " + "Kitap Adı : " + v);
+        }
+
+        public T[] itemkey
+        {
+            get { return key; }
+        }
+
+        public V[] itemvalue
+        {
+            get { return value; }
+        }
+
+        public int Lenght
+        {
+            get { return key.Length; }
+        }
+    }
+}
---
+++ MyDictionaryHomework/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MyDictionaryHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> bookName = new MyDictionary<string, string>();
+
+            bookName.Add("Stefan Zweig", "Satranç");
+            bookName.Add("Amin Maalouf", "Semerkant");
+            bookName.Add("Dan Brown", "Dijital Kale");
+
+
+
+            Console.WriteLine(bookName.Lenght);
+
+
+            foreach (var writer in bookName.itemkey)
+            {
+                Console.WriteLine(writer);
+            }
+
+            foreach (var bookname in bookName.itemvalue)
+            {
+                Console.WriteLine(bookname);
+            }
+        }
+    }
+}
